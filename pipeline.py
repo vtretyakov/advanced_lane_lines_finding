747fcd54eed30656d08c6cc34512b2cc36c17f61
vtretyakov <vtetyakov@synapticon.com>
2017-06-17 20:42:19 +0200

    move histogram computation to the window search method

--- pipeline.py
+++ pipeline.py
@@ -9,8 +9,8 @@ from moviepy.editor import VideoFileClip
 left_line = Line()
 right_line = Line()
 
+clip = VideoFileClip("project_video.mp4")#.subclip(38,43)#.subclip(20,25)
 
-clip = VideoFileClip("project_video.mp4")
 def running_average(buffer, current_fit):
     average = np.array([0,0,0], dtype='float')
     for i in range(len(buffer)):
@@ -81,18 +81,6 @@ def process_image(image):
         warped = cv2.line(warped,(1108-100,0),(1108-100,720),(255,0,0),5)
         #cv2.imwrite('output_images/annotated_lines_after_perspective_transform.jpg',warped)
         cv2.imshow('Warped image', warped)
-
-    # Take a histogram of the bottom half of the image
-    histogram = np.sum(merged_binary[merged_binary.shape[0]//2:,:], axis=0)
-
-    # Create an output image to draw on and  visualize the result
-    out_img = np.dstack((merged_binary, merged_binary, merged_binary))*255
-
-    # Find the peak of the left and right halves of the histogram
-    # These will be the starting point for the left and right lines
-    midpoint = np.int(histogram.shape[0]/2)
-    leftx_base = np.argmax(histogram[:midpoint])
-    rightx_base = np.argmax(histogram[midpoint:]) + midpoint
     
     # Create empty lists to receive left and right lane pixel indices
     left_lane_inds = []
@@ -100,6 +88,17 @@ def process_image(image):
 
     # Processing first frame or if detection failed
     if (left_line.detected == False) & (right_line.detected == False):
+        # Take a histogram of the bottom half of the image
+        histogram = np.sum(merged_binary[merged_binary.shape[0]//2:,:], axis=0)
+    
+        # Create an output image to draw on and  visualize the result
+        #out_img = np.dstack((merged_binary, merged_binary, merged_binary))*255
+    
+        # Find the peak of the left and right halves of the histogram
+        # These will be the starting point for the left and right lines
+        midpoint = np.int(histogram.shape[0]/2)
+        leftx_base = np.argmax(histogram[:midpoint])
+        rightx_base = np.argmax(histogram[midpoint:]) + midpoint
 
         # Choose the number of sliding windows
         nwindows = 9
@@ -129,8 +128,8 @@ def process_image(image):
             win_xright_low = rightx_current - margin
             win_xright_high = rightx_current + margin
             # Draw the windows on the visualization image
-            cv2.rectangle(out_img,(win_xleft_low,win_y_low),(win_xleft_high,win_y_high),(0,255,0), 2)
-            cv2.rectangle(out_img,(win_xright_low,win_y_low),(win_xright_high,win_y_high),(0,255,0), 2)
+            #cv2.rectangle(out_img,(win_xleft_low,win_y_low),(win_xleft_high,win_y_high),(0,255,0), 2)
+            #cv2.rectangle(out_img,(win_xright_low,win_y_low),(win_xright_high,win_y_high),(0,255,0), 2)
             # Identify the nonzero pixels in x and y within the window
             good_left_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) & (nonzerox >= win_xleft_low) & (nonzerox < win_xleft_high)).nonzero()[0]
             good_right_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) & (nonzerox >= win_xright_low) & (nonzerox < win_xright_high)).nonzero()[0]
@@ -193,7 +192,7 @@ def process_image(image):
 
         ### Next frame visualization
         # Create an image to draw on and an image to show the selection window
-        window_img = np.zeros_like(out_img)
+        #window_img = np.zeros_like(out_img)
         # Generate x and y values for plotting
         ploty = np.linspace(0, merged_binary.shape[0]-1, merged_binary.shape[0] )
         left_fitx = left_line.current_fit[0]*ploty**2 + left_line.current_fit[1]*ploty + left_line.current_fit[2]
@@ -274,7 +273,7 @@ def process_image(image):
     #Annotate image
     font = cv2.FONT_HERSHEY_SIMPLEX
     cv2.putText(result,"left_curverad {0:4.1f} right_curverad {1:4.1f}".format(left_curverad, right_curverad),(300,100), font, 1,(255,255,255),2)
-    cv2.putText(result,"center offset {0:4.1f} mtrs".format(center_offset_mtrs),(450,150), font, 1,(255,255,255),2)
+    cv2.putText(result,"center offset {0:4.2f} mtrs".format(center_offset_mtrs),(450,150), font, 1,(255,255,255),2)
 
     return result
 
