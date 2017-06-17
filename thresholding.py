1df2302edee96531834b1e46d78bdba419244f4d
vtretyakov <vtetyakov@synapticon.com>
2017-06-17 12:48:42 +0200

    add green channel threshold and sanity check s channel

--- thresholding.py
+++ thresholding.py
@@ -3,16 +3,13 @@ from matplotlib import pyplot as plt
 import numpy as np
 import pickle
 
-def thresholding_pipeline(img, s_thresh=(170, 255), sx_thresh=(20, 100)):
+def thresholding_pipeline(img, s_thresh=(170, 255), sx_thresh=(20, 100)): #170, 255
     img = np.copy(img)
+    img2 = np.copy(img)
     # Convert to HSV color space and separate the V channel
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HLS).astype(np.float)
     l_channel = hsv[:,:,1]
     s_channel = hsv[:,:,2]
-    #s_channel_test = np.copy(s_channel)
-    #binary_output = np.zeros_like(s_channel_test)
-    #binary_output[(s_channel_test > 150) & (s_channel_test <= 255)] = 1
-    #cv2.imshow('s_channel', binary_output)
     
     # Sobel x
     sobelx = cv2.Sobel(l_channel, cv2.CV_64F, 1, 0) # Take the derivative in x
@@ -26,10 +23,16 @@ def thresholding_pipeline(img, s_thresh=(170, 255), sx_thresh=(20, 100)):
     # Threshold color channel
     s_binary = np.zeros_like(s_channel)
     s_binary[(s_channel >= s_thresh[0]) & (s_channel <= s_thresh[1])] = 1
+    mean_s = np.mean(s_binary)
+    # Skip the channel if it has an abnormal dominance
+    if mean_s > 0.05:
+        s_binary = np.zeros_like(s_channel)
+    g_channel = img2[:,:,1]
+    g_channel_binary = np.zeros_like(g_channel)
+    g_channel_binary[(g_channel > 200) & (g_channel <= 255)] = 1
+    #cv2.imshow('g_channel', g_channel_binary*255)
     # Stack each channel
-    # Note color_binary[:, :, 0] is all 0s, effectively an all black image. It might
-    # be beneficial to replace this channel with something else.
-    color_binary = np.dstack(( np.zeros_like(sxbinary), sxbinary, s_binary))
+    color_binary = np.dstack(( g_channel_binary, sxbinary, s_binary))
     return color_binary
 
 def region_of_interest(img, vertices):
@@ -62,7 +65,7 @@ enable_test = False
 
 if enable_test == True:
     
-    image = cv2.imread('test_images/test4.jpg')
+    image = cv2.imread('test_images/test12.jpg')
     
     #Load camera calibration parameters
     cal_params = pickle.load(open('camera_cal/dist_pickle.p', 'rb'))
@@ -81,15 +84,15 @@ if enable_test == True:
     roi_applied = region_of_interest(threshold_applied, vertices)
 
     #Display results
-    #cv2.imwrite('output_images/threshold_applied.jpg',threshold_applied*255)
+    #cv2.imwrite('output_images/threshold_applied_with_g_t4.jpg',threshold_applied*255)
     cv2.imshow('Threshold applied', threshold_applied)
     #cv2.imwrite('output_images/roi_applied.jpg',roi_applied*255)
-    cv2.imshow('ROI applied', roi_applied)
+    #cv2.imshow('ROI applied', roi_applied)
 
     annotated = cv2.line(roi_applied,(216,720),(595,450),(0,0,255),2)
     annotated = cv2.line(annotated,(1108,720),(689,450),(0,0,255),2)
     annotated = cv2.line(annotated,(595,450),(689,450),(0,0,255),2)
-    cv2.imshow('Annotated lines', annotated)
+    #cv2.imshow('Annotated lines', annotated)
 
     cv2.waitKey()
     cv2.destroyAllWindows()
